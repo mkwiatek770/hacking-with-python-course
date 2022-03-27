dac2ede55ec27f280a83da24e87813caeb8ec97a
mkwiatek770 <michalakwiatek@gmail.com>
2022-03-27 17:49:02 +0200

    Get banner

--- port_scanner.py
+++ port_scanner.py
@@ -1,19 +1,26 @@
 # Use: scan other machine(s) and tell wether is has open port(s)
+# Example test site: http://testphp.vulnweb.com/
 
 
 import socket
+from typing import Tuple
 from IPy import IP
 
 
-def scan_port(ip_address, port) -> bool:
+def scan_port(ip_address, port) -> Tuple[bool, str]:
     try:
         sock = socket.socket()
         sock.settimeout(0.5)
         # time to connect to different ports differs
         sock.connect((ip_address, port))
-        return True
+        try:
+            banner = sock.recv(1024)
+            return True, banner.decode().strip('\n')
+        except:
+            return True, ''
     except:
-        False
+        return False, ''
+
 
 def convert_ip(ip_addr):
     try:
@@ -28,8 +35,11 @@ if __name__ == '__main__':
         print('Scanning: ', ip_address)
         open_ports_counter = 0
         for port in range(1, 100):
-            if scan_port(convert_ip(ip_address.strip()), port):
+            res, banner = scan_port(convert_ip(ip_address.strip()), port)
+            if res:
                 print(f'[+] {ip_address} Port {port} is open')
+                if banner:
+                    print('f Banner: {banner}')
                 open_ports_counter += 1
         if open_ports_counter == 0:
-            print(f'[-] {ip_address} no ports from range [1-100] are open')
+            print(f'[-] {ip_address} no ports from range [75-85] are open')
